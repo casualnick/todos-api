49d511ebdb7e8279109adcf83e742249610ee800
MrFrozeN <bartekstafiej2@gmail.com>
2020-08-09 15:54:02 +0200

    mistake

---
+++ spec/rails_helper.rb
@@ -0,0 +1,61 @@
+require 'database_cleaner'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configuration do |config|
+
+  config.include RequestSpecHelper, type: :request
+
+end
+
+RSpec.configure do |config|
+  
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include RequestSpecHelper, type: :request
+
+  config.use_transactional_fixtures = true
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
+ end
+
+    config.before(:suite) do
+      DatabaseCleaner.clean_with(:transaction)
+      DatabaseCleaner.strategy = :transaction
+    end
+
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
+    end
+  
+  
+  config.infer_spec_type_from_file_location!
+
+  config.filter_rails_from_backtrace!
+  
+end
